5c0dcfc2a2d9c6c38d0df8a5a4f8415093d49d5d
Vlad Semenenko <vlad_semenenko_2016@mail.ru>
2024-08-23 16:12:00 +0300

    feat: burger interaction on both pages

--- shelter/index.js
+++ shelter/index.js
@@ -1,2 +1,53 @@
-let text="SHELTER PART 2\nCделано на 100/100, пишите мне в дискорд @31ll1ax если что-то не так\nМикро недочеты в <10px имеются, но это допустимо\nПостарался всю верстку сделать резиновой\nЕдинственное, что не при 767px, а только лишь начиная с 766 прячется не все (например бургер-меню), хотя должно, надеюсь не критично";
+let text="SHELTER PART 3";
 console.log(text);
+
+let hamburgerButton = document.getElementsByClassName("header-hamburger-button")[0];
+let headerLinks = document.getElementsByClassName("header-links")[0];
+let hamburgerIcon = document.getElementsByClassName("hamburger-icon")[0];
+let hamburgerOpened = 1;
+const hamburgerWidth = 320;
+const hamburgerIconDegree = 45;
+let blur = document.getElementsByClassName("blur")[0];
+
+hamburgerButton.addEventListener('click', function() { Hamburger() });
+blur.addEventListener('click', function() { Hamburger() });
+let headerLink = document.getElementsByClassName("header-link");
+[...headerLink].forEach(element => {
+    element.addEventListener('click', function() { Hamburger() });
+});
+
+window.addEventListener('resize', function() { 
+    console.log(hamburgerOpened);
+    if (window.innerWidth >= 768){
+        blur.style.display = 'none';
+        document.documentElement.style.overflowY = "auto";
+        headerLinks.style.marginRight = `0px`;
+        hamburgerIcon.style.transform = `rotate(0deg)`; 
+    }
+    else if (hamburgerOpened === -1) {
+        blur.style.display = 'block';
+        hamburgerIcon.style.transform = `rotate(90deg)`; 
+    }
+    else {
+        headerLinks.style.marginRight = `-640px`;
+    }
+});
+    
+function Hamburger(){
+    if(window.innerWidth <= 768){
+         if (hamburgerOpened === 1){
+            blur.style.display = 'block';
+            document.documentElement.style.overflowY = "hidden";
+        }
+        else{
+            blur.style.display = 'none';
+            document.documentElement.style.overflowY = "auto";
+        } 
+        headerLinks.style.marginRight = (hamburgerOpened * hamburgerWidth) - 320 + `px`;
+        hamburgerOpened *= -1;
+        hamburgerIcon.style.transform = `rotate(` + (hamburgerIconDegree - hamburgerOpened * hamburgerIconDegree) + `deg)`; 
+    }
+}
+function Slider(){}
+function Pagination(){}
+function PopUp(){}
